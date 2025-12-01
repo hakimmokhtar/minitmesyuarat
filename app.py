465902d4d3a761a3f77ac7ffd4f2d6b3476be45c
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-12-02 03:59:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -220,8 +220,9 @@ if st.button("Generate PDF"):
         st.warning("Sila isi nama SU sebelum generate PDF.")
     else:
         st.success("PDF berjaya dihasilkan.")
-        st.download_button("Muat Turun Minit (PDF)", data=pdf_buf,
+        st.download_button("Muat Turun Minit (PDF)",
                            file_name=f"minit_BIL{bil or 'x'}_{tarikh}.pdf", mime="application/pdf")
+
 
 
 
