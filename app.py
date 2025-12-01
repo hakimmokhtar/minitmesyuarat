d179f6e8a1a87fec95f4ded8257e8b839c339857
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-12-02 01:00:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -178,9 +178,9 @@ elems.append(Spacer(1,8))
 
     # Agenda list
 elems.append(Paragraph("<b>AGENDA</b>", h2))
-    for i, ag in enumerate(agenda, start=1):
-        elems.append(Paragraph(f"{i}) {ag['title']}", normal))
-    elems.append(Spacer(1,8))
+for i, ag in enumerate(agenda, start=1):
+elems.append(Paragraph(f"{i}) {ag['title']}", normal))
+elems.append(Spacer(1,8))
 
     # Perbincangan
 elems.append(Paragraph("<b>PERBINCANGAN</b>", h2))
@@ -225,6 +225,7 @@ if st.button("Generate PDF"):
         st.success("PDF berjaya dihasilkan.")
         st.download_button("Muat Turun Minit (PDF)", data=pdf_buf,
                            file_name=f"minit_BIL{bil or 'x'}_{tarikh}.pdf", mime="application/pdf")
+
 
 
 
