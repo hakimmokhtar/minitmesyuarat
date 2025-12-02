176c32fec2c41ca8474e874318e427fd31a039e1
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-12-02 19:33:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -225,8 +225,8 @@ if st.button("Generate PDF"):
 
     else:
         st.success("PDF berjaya dihasilkan.")
-        st.download_button("Muat Turun Minit (PDF)", data=pdf_buf,
                            file_name=f"minit_BIL{bil or 'x'}_{tarikh}.pdf", mime="application/pdf")
+
 
 
 
