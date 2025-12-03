d4e436fd2ac5f68782a93c20bbe7a3fc9e064bbf
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-12-03 11:16:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -222,10 +222,10 @@ def build_pdf(logo_file=None, bg_file=None):
     elems.append(Paragraph("Disediakan oleh:", normal))
     elems.append(Spacer(1,20))
     elems.append(Paragraph(f"<b>{sign_anda}</b>", signature_style))
-    sign_line = "_ _ _ _ _ _ _ _ _ _"
+    sign_line = "____________________"
     elems.append(Paragraph(sign_line, normal))
-    elems.append(Paragraph(f"<b>{nama_anda}</b>", normal))
-    elems.append(Paragraph(f"<b>{jawatan_anda}</b>", normal))
+    elems.append(Paragraph(f"<b>{nama_anda}.upper</b>", normal))
+    elems.append(Paragraph(f"<b>{jawatan_anda}.upper</b>", normal))
 
     doc.build(
     elems, 
@@ -246,6 +246,7 @@ if st.button("Generate PDF"):
         st.success("PDF berjaya dihasilkan.")
         st.download_button("Muat Turun Minit (PDF)", data=pdf_buf,
                            file_name=f"minit_BIL{bil or 'x'}_{tarikh}.pdf", mime="application/pdf")
+
 
 
 
